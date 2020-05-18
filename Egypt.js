b56adb209c4108c7264b1aa805a3f9dd7f3664cf
Ilya <ilya.trofimov.01@gmail.com>
2020-05-18 03:19:20 +0300

    Update Weather render

--- Egypt.js
+++ Egypt.js
@@ -1,24 +1,21 @@
 var url = "https://api.openweathermap.org/data/2.5/weather?q=Cairo,eg&appid=4869a7a8271ae1b636c0c0f0f5140659&lang=ru";
-axios.post(url)
-	 .then(response => {
-	 	console.log(response)
-	 	let currentWind = response.data.wind.speed;
-	 	let currentTemp = response.data.main.temp;
-	 	let currentHumidity = response.data.main.humidity;
-	    let imgWeather = response.data.weather["0"].main;
-		currentTemp = currentTemp - 273.15;
-		
-		//var sunset = new Date(response.data.sys.sunset);
-		
-		 var today = new Date(); 
-		  today = today.getHours();
-		// console.log(sunset);
-		// console.log(today);	
-		// var sunrise = new Date(response.data.sys.sunrise);	
-		//console.log(sunrise);
+var day;
+var night;
+let imgWeather;
+fetch("http://localhost:8090/").then((data) => data.json()).then((res) =>{
+	let tmp;	
+	tmp = res.find((el) => el.id === 'Egypt');
+	day = tmp.sunrise;
+ 	night = tmp.sunset;
+});
+
+
+function gg () {
+	var today = new Date(); 
+		today = today.getHours();
 		switch(imgWeather){
 			case "Clear" : 
-				if (today < 18 && today > 6 ){
+				if (today < night && today > day ){
 					document.getElementById("weatherImageDisplay").innerHTML = `<img id = "weatherPhoto" src="Images/WeatherForecastImages/sun.png" alt="weatherForecastImg">`;
 					console.log('day')
 				}else {
@@ -28,7 +25,7 @@ axios.post(url)
 				
 			break;
 			case "Clouds": 
-			if (today > 18 && today < 6){
+			if (today > night && today < day){
 				document.getElementById("weatherImageDisplay").innerHTML = `<img id = "weatherPhoto" src="Images/WeatherForecastImages/cloudy.png" alt="weatherForecastImg">`;
 				console.log('day')
 			}else{
@@ -40,11 +37,18 @@ axios.post(url)
 			case "Thunderstorm":
 				document.getElementById("weatherImageDisplay").innerHTML = `<img id = "weatherPhoto" src="Images/WeatherForecastImages/thunder.png" alt="weatherForecastImg">`;
 			break;
-		}
-		
-		
-		
-		 
+		};
+};
+
+
+axios.post(url)
+	 .then(response => {
+	 	console.log(response)
+	 	let currentWind = response.data.wind.speed;
+	 	let currentTemp = response.data.main.temp;
+	 	let currentHumidity = response.data.main.humidity;
+	    imgWeather = response.data.weather["0"].main;
+		currentTemp = currentTemp - 273.15;	 
 		console.log (response.data.weather[0].description); // уже на русском говорит о текущей погоде
 		console.log(imgWeather);
 
@@ -52,6 +56,7 @@ axios.post(url)
 	 	document.getElementById("currentWind").innerHTML = currentWind;
 		 document.getElementById("currentHumidity").innerHTML = currentHumidity;
 		 
-	 });
-	
+	}).then(() => gg());
+
+
 
